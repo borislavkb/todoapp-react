d8fcbe84ab780f9c0e0758d393c2b27520ef9711
Borislav Borislavov <borislavkb@gmail.com>
2021-07-16 12:47:25 +0200

    Add basic structure, add form, input, button elements. Set state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import "./App.css";
+import { useState } from "react";
+import { setConstantValue } from "typescript";
 
-function App() {
+export default function App() {
+  const notes = ["cook", "code"];
+
+  const [data, setData] = useState();
+
+  const listNoteItem = data.map((textContent, index) => {
+    return <NoteListItem key={index} text={textContent} />;
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // console.log(event.target.noteItem.value);
+
+    const form = event.target;
+    const input = form.noteItem;
+    const newNoteItem = input.value;
+    console.log(newNoteItem);
+
+    const newNoteArr = [...data, newNoteItem];
+    setData(newNoteArr);
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1> To do App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="noteItem"
+          name="noteItem"
+          placeholder="Add new note"
+          required
+        />
+        <button type="Submit">Add</button>
+      </form>
+      <ul>{listNoteItem}</ul>
     </div>
   );
 }
 
-export default App;
+function NoteListItem({ text }) {
+  return <li>{text}</li>;
+}

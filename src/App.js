4c893d8739dfc5102d213a9600173f39c67452de
Borislav Borislavov <borislavkb@gmail.com>
2021-07-19 00:45:09 +0300

    Add delete functionality on note button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
 import { useState } from "react";
-import { setConstantValue } from "typescript";
 
 export default function App() {
-  const notes = ["cook", "code"];
-
   const [data, setData] = useState([]);
-
-  const listNoteItem = data.map((textContent, index) => {
-    return <NoteListItem key={index} text={textContent} />;
-  });
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -20,8 +13,36 @@ export default function App() {
     const newNoteItem = input.value;
     console.log(newNoteItem);
 
-    const newNoteArr = [...data, newNoteItem];
-    setData(newNoteArr);
+    const newNoteArr = [...data, { textContent: newNoteItem, isDone: false }];
+    console.log(newNoteArr);
+
+    setData(newNoteArr); // State changes
+    form.reset();
+  }
+  //Delete function
+
+  function handleDeleteNote(item) {
+    const deleteNotes = data.filter((item) => {
+      return item.textContent !== item.textContent;
+    });
+    setData(deleteNotes);
+  }
+
+  //Note render
+  const TodoItem = data.map((note, index, status) => {
+    return <NoteListItem key={index} item={note} status={status} />;
+  });
+
+  //Generate the note
+  function NoteListItem({ data, item, status, onClick }) {
+    return (
+      <div className="Note-div">
+        <button onClick={handleDeleteNote} className="Delete-btn">
+          X
+        </button>
+        <p className="noteContent">{item.textContent}</p>
+      </div>
+    );
   }
 
   return (
@@ -35,13 +56,11 @@ export default function App() {
           placeholder="Add new note"
           required
         />
-        <button type="Submit">Add</button>
+        <button className="App-btn" type="Submit">
+          Add
+        </button>
       </form>
-      <ul>{listNoteItem}</ul>
+      <div className="Note-box">{TodoItem}</div>
     </div>
   );
-}
-
-function NoteListItem({ text }) {
-  return <li>{text}</li>;
 }
